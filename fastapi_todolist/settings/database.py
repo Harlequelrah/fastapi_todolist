f64ac3e69a9281ae6c98a76c9a620283e9d0a34b
Harlequelrah <degbovimax19@gmail.com>
2024-12-26 03:51:14 +0000

    mise à jour de l'url de base de donnée

--- fastapi_todolist/settings/database.py
+++ fastapi_todolist/settings/database.py
@@ -6,7 +6,7 @@ from .secret import authentication
 # SQLALCHEMY_DATABASE_URL = (
 #     f"{authentication.connector}://{authentication.database_username}:{authentication.database_password}@{authentication.server}/{authentication.database_name}"
 # )
-SQLALCHEMY_DATABASE_URL ="mysql://root:eQOhfUfnTbdODstXThRMgyFlLjrhoQno@autorack.proxy.rlwy.net:53747/railway"
+SQLALCHEMY_DATABASE_URL ="mysql+mysqlconnector://root:eQOhfUfnTbdODstXThRMgyFlLjrhoQno@autorack.proxy.rlwy.net:53747/railway"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
