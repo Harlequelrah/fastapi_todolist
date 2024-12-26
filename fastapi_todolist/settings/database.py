913311d6cceea24a0843215df8aa7468d968636a
Harlequelrah <degbovimax19@gmail.com>
2024-12-26 12:30:20 +0000

    masquage de MYSQL_PUBLIC_URL

--- fastapi_todolist/settings/database.py
+++ fastapi_todolist/settings/database.py
@@ -2,11 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .secret import authentication
+import os
+SQLALCHEMY_DATABASE_URL = os.getenv("MYSQL_PUBLIC_URL")
+# Vérifier si la variable est bien définie
+if not SQLALCHEMY_DATABASE_URL:
+    print("not in production database")
+    SQLALCHEMY_DATABASE_URL = (
+    f"{authentication.connector}://{authentication.database_username}:{authentication.database_password}@{authentication.server}/{authentication.database_name}"
+    )
+print(f"Database URL: {SQLALCHEMY_DATABASE_URL}")
 
-# SQLALCHEMY_DATABASE_URL = (
-#     f"{authentication.connector}://{authentication.database_username}:{authentication.database_password}@{authentication.server}/{authentication.database_name}"
-# )
-SQLALCHEMY_DATABASE_URL ="mysql+mysqlconnector://root:eQOhfUfnTbdODstXThRMgyFlLjrhoQno@autorack.proxy.rlwy.net:53747/railway"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
