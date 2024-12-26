859d89c7e9dc0686a339fedea1f954816c61d46f
Harlequelrah <degbovimax19@gmail.com>
2024-12-26 03:37:55 +0000

    mise à jour de database_url

--- fastapi_todolist/settings/database.py
+++ fastapi_todolist/settings/database.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .secret import authentication
 
-SQLALCHEMY_DATABASE_URL = (
-    f"{authentication.connector}://{authentication.database_username}:{authentication.database_password}@{authentication.server}/{authentication.database_name}"
-)
+# SQLALCHEMY_DATABASE_URL = (
+#     f"{authentication.connector}://{authentication.database_username}:{authentication.database_password}@{authentication.server}/{authentication.database_name}"
+# )
+SQLALCHEMY_DATABASE_URL ="mysql://root:eQOhfUfnTbdODstXThRMgyFlLjrhoQno@autorack.proxy.rlwy.net:53747/railway"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
