f74f237b21d21f712ec2cf5c8ef5f2077759b904
Harlequelrah <degbovimax19@gmail.com>
2024-12-26 12:40:53 +0000

    optimisation pour utilisé deux base de données

--- fastapi_todolist/settings/database.py
+++ fastapi_todolist/settings/database.py
@@ -3,14 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .secret import authentication
 import os
-SQLALCHEMY_DATABASE_URL = os.getenv("MYSQL_PUBLIC_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("MYSQL_CONNECTOR_PUBLIC_URL")
 # Vérifier si la variable est bien définie
 if not SQLALCHEMY_DATABASE_URL:
-    print("not in production database")
     SQLALCHEMY_DATABASE_URL = (
     f"{authentication.connector}://{authentication.database_username}:{authentication.database_password}@{authentication.server}/{authentication.database_name}"
     )
-print(f"Database URL: {SQLALCHEMY_DATABASE_URL}")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
