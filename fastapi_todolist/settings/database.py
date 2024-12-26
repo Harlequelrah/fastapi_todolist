b3824762a22f278b2c6bb56abda557e58cdf2b9a
Harlequelrah <degbovimax19@gmail.com>
2024-12-26 13:07:29 +0000

    correctif concerant les url de base de données

--- fastapi_todolist/settings/database.py
+++ fastapi_todolist/settings/database.py
@@ -4,17 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from .secret import authentication
 import os
 
-print("Variables d'environnement disponibles :")
-for key, value in os.environ.items():
-    print(f"{key}: {value}")
+
 SQLALCHEMY_DATABASE_URL = os.getenv("MYSQL_CONNECTOR_PUBLIC_URL")
 # Vérifier si la variable est bien définie
 if not SQLALCHEMY_DATABASE_URL:
-    print("in prod")
+
     SQLALCHEMY_DATABASE_URL = (
     f"{authentication.connector}://{authentication.database_username}:{authentication.database_password}@{authentication.server}/{authentication.database_name}"
     )
-print(SQLALCHEMY_DATABASE_URL)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
