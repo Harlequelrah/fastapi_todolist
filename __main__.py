cae4955d5d0f00d5c6aa09f3bb1f7ab54a6ffcc2
Harlequelrah <degbovimax19@gmail.com>
2024-12-27 00:39:05 +0000

    ajout de l'authentification aux routes de logs

---
+++ __main__.py
@@ -0,0 +1,6 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "fastapi_todolist.__main__:app", host="127.0.0.1", port=8000, reload=True
+    )
--- fastapi_todolist/loggerapp/log_router.py
+++ fastapi_todolist/loggerapp/log_router.py
@@ -1,19 +1,25 @@
-from fastapi import APIRouter
-from fastapi_todolist.loggerapp.log_crud import logCrud
+from typing import List
+from fastapi import APIRouter , Depends
+from .log_crud import logCrud
+from fastapi_todolist.settings.database import authentication
+from .log_schema import LoggerMiddlewarePydanticModel as LMPD
 
 app_logger=APIRouter(
     tags=['logs'],prefix='/logs'
 )
 
 @app_logger.get('/get-count-logs')
-async def get_count_logs():
+async def get_count_logs(access_token:str=Depends(authentication.get_current_user)):
     return await logCrud.get_count_logs()
 
-@app_logger.get('/get-log/{log_id}')
-async def get_log(log_id:int):
+
+@app_logger.get("/get-log/{log_id}", response_model=LMPD)
+async def get_log(
+    log_id: int, access_token: str = Depends(authentication.get_current_user)
+):
     return await logCrud.get_log(log_id)
 
 
-@app_logger.get('/get-logs')
-async def get_logs(skip: int = 0, limit: int = None):
+@app_logger.get('/get-logs',response_model=List[LMPD])
+async def get_logs(access_token:str=Depends(authentication.get_current_user),skip: int = 0, limit: int = None):
     return await logCrud.get_logs(skip=skip, limit=limit)
