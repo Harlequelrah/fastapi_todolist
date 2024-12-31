dacb1c135f4350d1d68121835ad92cf7cfef9c0e
Harlequelrah <degbovimax19@gmail.com>
2024-12-31 21:21:39 +0000

    debogage

--- fastapi_todolist/todolistapp/route.py
+++ fastapi_todolist/todolistapp/route.py
@@ -6,24 +6,24 @@ from fastapi_todolist.todolistapp.crud import todo_crud
 import fastapi_todolist.todolistapp.model as model
 from fastapi import Depends, APIRouter
 from typing import List
-from harlequelrah_fastapi.router.router_provider import provide_router,ProvideRouter
+from harlequelrah_fastapi.router.route_config import RouteConfig
+from harlequelrah_fastapi.router.router_provider import CustomRouterProvider
 
-provide_router= ProvideRouter(
+router_provider= CustomRouterProvider(
     prefix="/todoitem",
     tags=["todo"],
     PydanticModel=TodoItem,
     crud=todo_crud,
-    authentication=authentication,
+    get_session=authentication.get_session,
+    get_access_token=authentication.get_access_token
 )
-provide_router.initialize_router(
-    {
-        'create':True,
-        'update':True,
-        'count':True,
-        'read-one':True,
-        'read-all':True,
-        'read-all-by-filter':True,
-        'delete':True
-    }
-)
-app_todolist=provide_router.router
+# app_todolist = router_provider.get_default_router()
+# app_todolist = router_provider.get_protected_router()
+
+init_data:List[RouteConfig]=[
+    RouteConfig(route_name='create',is_activated=True),
+    RouteConfig(route_name='read-one', is_activated=True),
+    RouteConfig(route_name='update', is_activated=True,is_protected=True),
+    RouteConfig(route_name='delete', is_activated=True,is_protected=True),
+]
+app_todolist= router_provider.initialize_router(init_data=init_data)
